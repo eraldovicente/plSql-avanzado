23331006034627073e2ce6555b0cce7743a17071
eraldovicente <eraldovicente2@gmail.com>
2022-05-25 18:31:07 -0300

    INTRODUCIR DATOS COMPUESTOS EN UNA INDEX BY TABLE

--- aula-14.sql
+++ aula-14.sql
@@ -16,9 +16,14 @@ BEGIN
     depts(50)     := 'OTRO VALOR';
     depts(-10)    := 'SIGUIENTE VALOR';
     nombres('AA') := 'ERALDO';
+    
+    EMPLES(10).FIRST_NAME := 'BIANA';
+    
     dbms_output.put_line(depts(2));
     dbms_output.put_line(depts(-10));
     dbms_output.put_line(nombres('AA'));
+    dbms_output.put_line(EMPLES(10).FIRST_NAME);
+    dbms_output.put_line(EMPLES(10).LAST_NAME);
 END;
 /
 --SPARSE ARRAY
