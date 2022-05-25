63b8b39a7edf256c6ca75d0107e8bde103b05a99
eraldovicente <eraldovicente2@gmail.com>
2022-05-25 14:39:16 -0300

    TRABAJAR CON CURSORES VARIABLES PARTE 2

--- aula-08.sql
+++ aula-08.sql
@@ -3,9 +3,10 @@ SET SERVEROUTPUT ON;
 DECLARE
 -- CURSOR C1 IS SELECT * FROM EMPLOYEES;
     TYPE cursor_variable IS REF CURSOR;
-    v1        cursor_variable;
-    x1        cursor_variable;
-    empleados employees%rowtype;
+    v1            cursor_variable;
+    x1            cursor_variable;
+    empleados     employees%rowtype;
+    departamentos departments%rowtype;
 BEGIN
     OPEN v1 FOR SELECT
                     *
@@ -17,4 +18,11 @@ BEGIN
                          || ' '
                          || empleados.salary);
     CLOSE v1;
+    OPEN v1 FOR SELECT
+                    *
+                FROM
+                    departments;
+
+    FETCH v1 INTO departamentos;
+    dbms_output.put_line(departamentos.department_name);
 END;
