42b2e3daca25ec8109092efeba802f71685a1a2e
eraldovicente <eraldovicente2@gmail.com>
2022-05-26 10:12:05 -0300

    MÉTODOS DE LAS COLECCIONES

---
+++ aula-18.sql
@@ -0,0 +1,14 @@
+--------------------------------
+-- Métodos de las colecciones --
+--------------------------------
+
+DELETE - Procedure - Elimina elementos de una tabla anidada o tabla indexada.
+TRIM - Procedure - Elimina elementos desde el final de un varray o tabla anidada.
+EXTEND - Procedure - Agrega elementos al final de un varray o tabla anidada.
+EXISTS - Function - Retorna TRUE si y solo si existe el elemento especificado de la colección.
+FIRST - Function - Retorna el primer índice de la colección.
+LAST - Function - Retorna el último índice de la colección.
+COUNT - Function - Retorna el número de elementos en la colección.
+LIMIT - Function - Retorna el número máximo de elementos que la colección puede tener.
+PRIOR - Function - Retorna el índice que precede al índice especificado.
+NEXT - Function - Retorna el índice siguiente al índice especificado.
