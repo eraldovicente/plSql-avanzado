14f14297985bd376b867a747c5de1a6bd0cae86f
eraldovicente <eraldovicente2@gmail.com>
2022-05-27 16:28:48 -0300

    CREAR UNA TABLA CON COLUMNAS NESTED TABLES. PRÁCTICA

---
+++ aula-29.sql
@@ -0,0 +1,16 @@
+DESC EMPLEADO;
+
+CREATE TABLE emples (
+    codigo    NUMBER,
+    direccion VARCHAR2(100),
+    datos     empleado
+)
+NESTED TABLE datos STORE AS tabla_datos;
+/
+
+DESC EMPLES;
+
+SELECT
+    *
+FROM
+    tab;
