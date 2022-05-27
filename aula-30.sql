531968822f0784d38b7986b4f7939eb56083cf98
eraldovicente <eraldovicente2@gmail.com>
2022-05-27 16:53:17 -0300

    TRABAJAR CON TABLAS CON COLUMNAS NESTED TABLES

---
+++ aula-30.sql
@@ -0,0 +1,31 @@
+DESC EMPLES;
+
+INSERT INTO emples VALUES (
+    1,
+    'PINTORES 5',
+    empleado(
+        'PEPE'
+    )
+);
+
+SELECT
+    *
+FROM
+    emples;
+
+--SELECT * FROM TABLA_DATOS;
+
+--DROP TABLE TABLA_DATOS;
+
+INSERT INTO emples VALUES (
+    2,
+    'CALLE 2',
+    empleado(
+        'PEPE', 'RODRIGUEZ MATINEZ'
+    )
+);
+
+SELECT
+    *
+FROM
+    emples;
