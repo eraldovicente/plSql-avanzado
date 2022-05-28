f0f6224fc2030adb6aca5803a58fcece5a2bfabb
eraldovicente <eraldovicente2@gmail.com>
2022-05-28 20:51:15 -0300

    BFILE: EJEMPLO PRÁCTICO PARTE 1

---
+++ aula-37.sql
@@ -0,0 +1,35 @@
+CREATE TABLE clientes (
+    codigo NUMBER,
+    nombre VARCHAR2(100),
+    foto   BFILE,
+    longitud    NUMBER
+);
+/
+
+INSERT INTO clientes VALUES (
+    1,
+    'ROSA',
+    NULL,
+    NULL
+);
+
+INSERT INTO clientes VALUES (
+    2,
+    'PEDRO',
+    NULL,
+    NULL
+);
+
+INSERT INTO clientes VALUES (
+    3,
+    'ANTONIO',
+    NULL,
+    NULL
+);
+
+INSERT INTO clientes VALUES (
+    5,
+    'MARIA',
+    NULL,
+    NULL
+);
